d0a9711d8e1db6722dc6c74cc3d576c6703093f0
CodingKL <50916645+CodingKL@users.noreply.github.com>
2022-12-11 20:37:34 +0900

    alkguitoh

---
+++ Windows/powershell/WpgFitter/pss/IeFitter.ps1
@@ -0,0 +1,71 @@
+﻿
+# Set URL
+$Url = "https://www.plexonline.com"; 
+#$url = "https://www.baidu.com"; 
+#$URL = "https://qiita.com/"; 
+#$URL = "https://qiita.com/login/"; 
+
+#$Username = "xxxx";
+#$Password = "xxxx";
+#$CompanyCode = "xxx";
+$Username = "TestSelenium";
+$Password = "Mki123@zyh";
+
+# 为IE创建一个新的COM对象。
+#$IE = New-Object -ComObject internetexplorer.application;
+#$IE = New-Object -ComObject InternetExplorer.application;
+$IE = New-Object -ComObject InternetExplorer.Application;
+
+# 将IE设置为全屏。
+$IE.FullScreen = $true;
+# 将IE设置为可见。
+$IE.Visible = $true;
+
+# 将IE导航到指定的URL。
+#$IE.navigate($url); 
+$IE.Navigate($url); 
+#$ie.Navigate("C:\users\310080829\Desktop\test.html")
+#$ie.Navigate("D:\github\Scripts_APLxy\Windows\powershell\WpgFitter\html\test.html")
+#$ie.Navigate("D:\github\Scripts_APLxy\Windows\powershell\WpgFitter\html\runoob-tryhtml_link_target.html")
+#$ie.Navigate("D:\github\Scripts_APLxy\Windows\powershell\WpgFitter\html\runoob-tryhtml_link_locations.html")
+
+
+#$IE.Navigate2("https://stackoverflow.com");
+
+# 等到IE不再繁忙。
+while ($IE.Busy -eq $true) 
+{
+    Start-Sleep -Milliseconds 5000; 
+} 
+Start-Sleep -Milliseconds 5000; 
+# 将指定的用户名、密码和公司代码插入到各自的字段中。
+$IE.Document.getElementById("txtUserID").value = $Username;
+$IE.Document.getElementByID("txtPassword").value = $Password;
+#$IE.Document.getElementByID("txtCompanyCode").value = $CompanyCode;
+
+#$IE.Document.getElementById("identity").value = $Username;
+
+#$link = $ie.Document.getElementsByTagName('A') | where-object {$_.innerText -eq 'ログイン'}
+#$link = $IE.Document.getElementsByTagName('a')  |  Where-Object {$_.href -eq 'login'} 
+
+#$link = $IE.Document.getElementsByTagName('a') 
+
+#$link = $IE.Document.getElementsByTagName('a')
+#$temp = $IE.document.getElementsByTagName('h1');
+
+#$link = $IE.Document.getElementsByTagName('a') | Where-Object {$_.href -eq '#C4'} 
+#$link.click() 
+
+#$link = $doc.getElementsByTagName('a') | Where-Object {$_.href -match '`#r1r3'} 
+#$link = $IE.getElementsByTagName('h1') | Where-Object {$_.href -match '`#C4'} 
+#$link.click() 
+
+# 开始登录过程。
+#$IE.Document.getElementById("btnLogin_Label").Click();
+
+#$titles = $ie.Document.body.getElementsByClassName('newstitle')
+$titles = $ie.Document.body.getElementsByTagName('p')
+foreach ($storyTitle in $titles) {
+     Write-Output $storyTitle.innerText
+}
+
---
+++ Windows/powershell/WpgFitter/pss/WpgFitter.ps1
@@ -0,0 +1,75 @@
+﻿
+
+$WebsiteCfgArr = @{}
+
+#Read config information from file
+Import-Csv ..\cfg\WebsiteInfo.cfg -header Key,Val -Delimiter "=" | % { $WebsiteCfgArr.Add($_.Key.Trim(), $_.Val.Trim())}
+
+# https://www.tutorialspoint.com/powershell/powershell_array.htm
+write-host("Print all the config information:")
+$WebsiteCfgArr
+Write-Host "`n"
+
+$myList = 5.6, 4.5, 3.3, 13.2, 4.0, 34.33, 34.0, 45.45, 99.993, 11123
+write-host("Print all the array elements")
+$myList
+
+write-host("Get the length of array")
+$myList.Length
+
+write-host("Get Second element of array")
+$myList[1]
+
+write-host("Get partial array")
+$subList = $myList[1..3]
+
+write-host("print subList")
+$subList
+
+write-host("using for loop")
+for ($i = 0; $i -le ($myList.length - 1); $i += 1) {
+  $myList[$i]
+}
+
+write-host("using forEach Loop")
+foreach ($element in $myList) {
+  $element
+}
+
+write-host("using while Loop")
+$i = 0
+while($i -lt 4) {
+  $myList[$i];
+  $i++
+}
+
+write-host("Assign values")
+$myList[1] = 10
+$myList
+
+# 从 PowerShell 脚本启动 Internet Explorer
+# https://www.it1352.com/2340797.html
+
+$logFileName = "log_" + (Get-Date).ToString('yyyyMMdd-HHmmss') + ".log"
+Start-Transcript ..\log\$logFileName
+
+$startIeCmd = New-Object -ComObject InternetExplorer.Application
+$startIeCmd.Visible = $true
+#$startIeCmd.Navigate2("https://stackoverflow.com");
+
+# https://blog.csdn.net/purvispanwu/article/details/115423213?spm=1001.2101.3001.6650.7&utm_medium=distribute.pc_relevant.none-task-blog-2%7Edefault%7EBlogCommendFromBaidu%7ERate-7-115423213-blog-127362343.pc_relevant_3mothn_strategy_and_data_recovery&depth_1-utm_source=distribute.pc_relevant.none-task-blog-2%7Edefault%7EBlogCommendFromBaidu%7ERate-7-115423213-blog-127362343.pc_relevant_3mothn_strategy_and_data_recovery&utm_relevant_index=14
+
+# https://www.zhblog.net/qa/to-use-chrome.html
+
+$Url = "https://www.plexonline.com"; 
+#$Username="xxxx";
+#$Password="xxxx";
+
+$Credential = New-Object System.Management.Automation.PSCredential -ArgumentList @($Username,(ConvertTo-SecureString -String $Password -AsPlainText -Force))
+
+
+# Start-Process "<path to chrome.exe>" $Url -Credential $Credential -WindowStyle Maximized
+#Start-Process "C:\Program Files\Google\Chrome\Application\chrome.exe" $Url -Credential $Credential -WindowStyle Maximized
+#Start-Process "C:\Program Files\Google\Chrome\Application\chrome.exe" $Url
+
+# post processing
